e61a67705399b9edbb066ecb38dbdb14ecbe5484
NicolasCarrera <nacarrera@uce.edu.ec>
2024-10-10 17:59:01 -0500

    update(api): Se añadieron anotaciones para la documentación del TestController

--- user-role-management-service/src/main/java/com/proyecto_titulacion/assettrack/controller/TestController.java
+++ user-role-management-service/src/main/java/com/proyecto_titulacion/assettrack/controller/TestController.java
@@ -1,29 +1,36 @@
 package com.proyecto_titulacion.assettrack.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
 @CrossOrigin(origins = "*")
+@Tag(name = "Test", description = "API para testar los métodos HTTP")
 public class TestController {
+    @Operation(summary = "Método GET para probar la API", description = "Retorna una respuesta de prueba para el método GET")
     @GetMapping
     public ResponseEntity<String> getMapping() {
         return ResponseEntity.ok("GET");
     }
+
+    @Operation(summary = "Método POST para probar la API", description = "Retorna una respuesta de prueba para el método POST")
     @PostMapping
     public ResponseEntity<String> postMapping(){
         return ResponseEntity.ok("POST");
     }
+
+    @Operation(summary = "Método PUT para probar la API", description = "Retorna una respuesta de prueba para el método PUT")
     @PutMapping
     public ResponseEntity<String> putMapping() {
         return ResponseEntity.ok("PUT");
     }
+
+    @Operation(summary = "Método DELETE para probar la API", description = "Retorna una respuesta de prueba para el método DELETE")
     @DeleteMapping
     public ResponseEntity<String> deleteMapping() {
-        return ResponseEntity.ok("Delete");
+        return ResponseEntity.ok("DELETE");
     }
 }
